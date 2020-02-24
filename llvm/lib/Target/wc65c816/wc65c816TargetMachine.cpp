1c5b21827b984b90a9b43b33f50b03802422d5ef
Connor Horman <chorman64@gmail.com>
2020-02-24 12:45:00 -0500

    Pull From Upstream

--- llvm/lib/Target/wc65c816/wc65c816TargetMachine.cpp
+++ llvm/lib/Target/wc65c816/wc65c816TargetMachine.cpp
@@ -12,5 +12,5 @@
 using namespace llvm;
 wc65c816TargetMachine::wc65c816TargetMachine(const Module& M,const std::string& FS)
     : layout("e-p:32:32-f128:16:16-i16:16"),
-    instructions(M,FS),
+    instructions(),
     frameInfo(TargetFrameInfo::StackGrowsDown,4,0){}
