18007bf489c456676a18d6adac64b69a74ba9bcf
Connor Horman <chorman64@gmail.com>
2020-02-24 08:42:26 -0500

    Start TargetInstrInfo

--- llvm/lib/Target/wc65c816/wc65c816TargetMachine.h
+++ llvm/lib/Target/wc65c816/wc65c816TargetMachine.h
@@ -6,12 +6,42 @@
 #define SNES_DEV_WC65C816TARGETMACHINE_H
 
 #include "llvm/Target/TargetMachine.h"
+#include "wc65c816Instructions.hpp"
 
 namespace llvm{
     class Module;
 
-    class wc65c816TargetMachine: TargetMachine{
+    class wc65c816TargetMachine: public LLVMTargetMachine{
+        const DataLayout layout;
+        wc65c816InstrInfo instructions;
+        TargetFrameInfo frameInfo;
+    protected:
+        virtual const TargetAsmInfo* createTargetAsmInfo()const;
+    public:
+        wc65c816TargetMachine(const Module &M,const std::string &Fs);
+        virtual const wc65c816InstrInfo* getInstrInfo() const{
+            return &instructions;
+        }
+        virtual const TargetSubtarget* getSubtargetImpl() const{
+            return nullptr;
+        }
+        virtual const TargetFrameInfo* getFrameInfo() const{
+            return &frameInfo;
+        }
+        virtual const TargetRegisterInfo* getRegisterInfo() const{
+            return &instructions.getRegisterInfo();
+        }
 
+        virtual const DataLayout* getDataLayout() const{
+            return &layout;
+        }
+
+        virtual const TargetSubtarget* getSubtargetImpl() const{
+            return nullptr;
+        }
+
+        virtual bool addInstSelector(PassManagerBase& PM, bool Fast);
+        virtual bool addPreEmitPass(PassManagerBase& PM, bool Fast);
     };
 }
 
